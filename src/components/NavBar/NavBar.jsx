efcccae0b88b244c8f5ea3a6038f3925e5269fa1
MazzeiFacundo <facumazzei@hotmail.com>
2022-06-28 19:01:04 -0500

    Polished responsive design

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,11 +7,17 @@ export default function NavBar() {
 
     return (
         <nav className={`${location.pathname === '/mazzeiPortfolio/summary' || '/mazzeiPortfolio/myProjects' ? "navSummary" : "nav"}`}>
-            <Link className="textNav" to='/mazzeiPortfolio'>Web Developer</Link>
+            {/* <Link className="textNav" to='/mazzeiPortfolio'>Web Developer</Link>
             <Link className="navText" to='/mazzeiPortfolio'>Home</Link>
             <Link className="navText" to='/mazzeiPortfolio/summary'>Summary</Link>
             <Link className="navText" to='/mazzeiPortfolio/myProjects'>Projects</Link>
-            <Link className="navText" to='/mazzeiPortfolio/contact'>Contact</Link>
+            <Link className="navText" to='/mazzeiPortfolio/contact'>Contact</Link> */}
+
+            <Link className="textNav" to='/'>Web Developer</Link>
+            <Link className="navText" to='/'>Home</Link>
+            <Link className="navText" to='/summary'>Summary</Link>
+            <Link className="navText" to='/myProjects'>Projects</Link>
+            <Link className="navText" to='/contact'>Contact</Link>
         </nav>
     )
 }
