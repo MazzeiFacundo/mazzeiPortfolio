0d63c808dcd5939d7365a3f939f0ed28b029b234
MazzeiFacundo <facumazzei@hotmail.com>
2022-07-29 15:58:29 -0500

    new project

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -4,6 +4,7 @@ import "./MyProjects.css"
 import clock from "./clock.gif"
 import bragi from "./BragiIMG.png"
 import codeScroll from "./codeScroll.gif"
+import reactImg from "./react-redux.png"
 
 function MyProjects() {
     return (
@@ -21,11 +22,12 @@ function MyProjects() {
                     </div>
                 </div>
                 <div className="projectDiv">
-                <img src={clock} className="insideDivIMGNotReady" alt = "none"></img>
+                <img src={reactImg} className="insideDivIMGInverter" alt = "none"></img>
                     <div className="projectTextContainer">
-                        <div className="projectTitle">Coming Soon</div>
-                        <div className="projectPreviewNotReady">Working on new projects!</div>
-                        <div className="projectDescriptionNotReady">Come back soon to see my next project once it´s completed.</div>
+                        <div className="projectTitle">Text inverter</div>
+                        <div className="projectPreview">Text inverter with Redux!</div>
+                        <div className="projectDescription">Type something to invert it and check if the word is a palindrome</div>
+                        <a className="projectDescriptionLink" href="https://cwr-facundo-mazzei-f.herokuapp.com/">Click here to visit the project!</a>
                     </div>
                 </div>
                 <div className="projectDiv">
